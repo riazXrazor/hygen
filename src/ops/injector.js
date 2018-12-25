d7240abfe4ea02102dcecee73dc2bb99402a2f8c
Riaz Laskar <riazcool77@gmail.com>
2018-12-25 21:23:08 +0530

    inject append fix

--- src/ops/injector.js
+++ src/ops/injector.js
@@ -1,10 +1,19 @@
 // @flow
 
-import type { RenderedAction } from '../types'
+import type {
+  RenderedAction
+} from '../types'
 const L = require('lodash')
 
 const injector = (action: RenderedAction, content: string): string => {
-  const { attributes: { skip_if, eof_last }, attributes, body } = action
+  const {
+    attributes: {
+      skip_if,
+      eof_last
+    },
+    attributes,
+    body
+  } = action
   const lines = content.split('\n')
   //eslint-disable-next-line
   const shouldSkip = skip_if && !!L.find(lines, _ => _.match(skip_if))
@@ -32,11 +41,11 @@ const before = (_, lines) => L.findIndex(lines, l => l.match(_))
 const locations = {
   at_line: _ => _,
   prepend: _ => 0,
-  append: (_, lines) => lines.length - 1,
+  append: (_, lines) => lines.length,
   before,
   after: (_, lines) => before(_, lines) + 1
 }
-const indexByLocation = (attributes: any, lines: Array<string>): number => {
+const indexByLocation = (attributes: any, lines: Array < string > ): number => {
   const pair = L.find(L.toPairs(attributes), ([k, v]) => locations[k])
   if (pair) {
     const [k, v] = pair
